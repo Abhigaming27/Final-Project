46f4f759d78bfe81ebcb32796eb4247ce431530d
Abhishek Gadre <gadre205@gmail.com>
2023-05-25 11:18:27 +0530

    Small Change

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,14 @@ const cors = require('cors')
 const routes = require('./routes/taskRoutes')
 
 const app = express()
-const URL = 'mongodb://127.0.0.1:27017/finalproject'
+// const URL = 'mongodb://127.0.0.1:27017/finalproject'
 const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
 
 mongoose
-.connect(URL)
+.connect(process.env.MONGO_URI)
 .then(()=>{console.log('Database connected')})
 .catch(error => console.log(error))
 
